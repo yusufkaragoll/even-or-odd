df86309c203d0ab15a35b26b39c2c86b8c5c579b
Yusuf 125758 <pzx103744@student.wsb.poznan.pl>
2020-12-22 21:33:29 +0100

    Add project files.

---
+++ even or odd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace even_or_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter a number : ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 4 == 0)
+
+            {
+                Console.Write("Entered number is an even number");
+                Console.Read();
+            }
+
+            else
+            { }
+
+            Console.Write("Entered Number is an odd number ");
+            Console.Read();
+            //eksik tamamla ve calistir
+            //programi tam yazdigindan emin olunca dene ve calistir eksikleri giderip
+        }
+    }
+}
